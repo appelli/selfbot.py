4603cc0f9d54093a52aeb4ec66bd5a5886392b72
jaxf <jacksonfoster87@gmail.com>
2017-10-29 10:51:02 -0400

    ModuleNotFoundError: No module named 'cogs.utils.checks'; 'cogs.utils' is not a package

--- cogs/google.py
+++ cogs/google.py
@@ -1,10 +1,19 @@
 import discord
 import json
 from discord.ext import commands
-from cogs.utils.checks import load_optional_config, embed_perms, get_google_entries
-from cogs.utils.config import get_config_value
 import aiohttp
 import urllib.parse
+import discord
+from discord.ext import commands
+from urllib.parse import urlparse
+from ext import embedtobox
+import datetime
+import asyncio
+import psutil
+import random
+import pip
+import os
+import io
 
 '''Module for google web and image search.'''
 
